bede34ee21fbd55cc7f9c0a8a19750fde997a3b0
Tony Froes <42457749+tonyfroes@users.noreply.github.com>
2023-12-12 15:04:02 -0300

    Automatico entrada de valores

--- functions.py
+++ functions.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 import csv
 
 def save_options(driver):
@@ -18,71 +17,80 @@ def save_options(driver):
     
     return opcoes_origem
 
-def select_source(driver, opcoes_origem):
-    escolha = int(input("Escolha uma origem pelo número: "))
-    if 1 <= escolha <= len(opcoes_origem):
+# def select_source(driver, opcoes_origem):
+#     escolha = int(input("Escolha uma origem pelo número: "))
+#     if 1 <= escolha <= len(opcoes_origem):
+#         select_element = Select(driver.find_element(By.ID, "origem"))
+#         select_element.select_by_index(escolha - 1)
+#         opcao_selecionada = select_element.first_selected_option.text
+#         print(f"Você selecionou: {opcao_selecionada}")
+#         return opcao_selecionada
+#     else:
+#         print("Escolha inválida. Por favor, escolha um número válido.")
+#         return None
+
+
+def select_source(driver, opcoes_origem, index):
+    if 0 <= index < len(opcoes_origem):
         select_element = Select(driver.find_element(By.ID, "origem"))
-        select_element.select_by_index(escolha - 1)
+        select_element.select_by_index(index)
         opcao_selecionada = select_element.first_selected_option.text
         print(f"Você selecionou: {opcao_selecionada}")
         return opcao_selecionada
     else:
-        print("Escolha inválida. Por favor, escolha um número válido.")
+        print("Índice inválido. Por favor, insira um número válido.")
         return None
 
-def test_and_save_image(driver, selected_option):
+def test_dados(driver, selected_option):
     with open('ips.csv', 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
-        next(csv_reader)
+        next(csv_reader)  # Skip header if present
 
         for row in csv_reader:
-            new_value = row[0]
+            if row:  # Ensure the row is not empty
+                new_value = row[0]
 
-            driver.execute_script("document.getElementById('ip').value = arguments[0];", new_value)
+                driver.execute_script("document.getElementById('ip').value = arguments[0];", new_value)
 
-            testar_button = driver.find_element(By.ID, "testar")
-            if testar_button.is_enabled() and testar_button.is_displayed():
-                testar_button.click()
-                print("Botão Testar clicado!")
+                testar_button = driver.find_element(By.ID, "testar")
+                if testar_button.is_enabled() and testar_button.is_displayed():
+                    testar_button.click()
+                    print("Botão Testar clicado!")
 
-                wait = WebDriverWait(driver, 120)
-                wait.until(EC.visibility_of_element_located((By.ID, "graph_end")))
+                    wait = WebDriverWait(driver, 120)
+                    wait.until(EC.visibility_of_element_located((By.ID, "graph_end")))
 
-                result_table = wait.until(EC.visibility_of_element_located((By.ID, "graph_table")))
+                    result_table = wait.until(EC.visibility_of_element_located((By.ID, "graph_table")))
 
-                wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, "tr")))
-                
-                wait.until(EC.presence_of_element_located((By.ID, "graph_end")))
+                    wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, "tr")))
 
-                print("Tabela de resultados exibida!")
-                rows = result_table.find_elements(By.TAG_NAME, "tr")[:-1]
+                    wait.until(EC.presence_of_element_located((By.ID, "graph_end")))
 
-                data_to_write = []
+                    print("Tabela de resultados exibida!")
+                    rows = result_table.find_elements(By.TAG_NAME, "tr")[:-1]
 
-                for row in rows:
-                    cells = row.find_elements(By.TAG_NAME, "td")
-                    if cells:
-                        hop = cells[0].text
-                        ms = cells[3].text if len(cells) > 3 else ""
-                        if ms != "FIM":
-                            selected_option = selected_option
-                            new_value = new_value
+                    data_to_write = []
 
+                    for row in rows:
+                        cells = row.find_elements(By.TAG_NAME, "td")
+                        if cells:
+                            hop = cells[0].text
+                            ms = cells[3].text if len(cells) > 3 else ""
+                            if ms != "FIM":
+                                selected_option = selected_option
+                                new_value = new_value
+
+                                data_to_write.append([selected_option, new_value, hop, ms])
+                        elif row.find_elements(By.TAG_NAME, "th"):
+                            th_elements = row.find_elements(By.TAG_NAME, "th")
+                            hop = th_elements[0].text
+                            ms = th_elements[3].text
                             data_to_write.append([selected_option, new_value, hop, ms])
-                    elif row.find_elements(By.TAG_NAME, "th"):
-                        th_elements = row.find_elements(By.TAG_NAME, "th")
-                        hop = th_elements[0].text
-                        ms = th_elements[3].text
-                        data_to_write.append([selected_option, new_value, hop, ms])
-                        print("Cabeçalho da tabela encontrado!")
-                csv_file = 'traceroute.csv'
+                            print("Cabeçalho da tabela encontrado!")
+                    csv_file = 'traceroute.csv'
 
-                with open(csv_file, 'a', newline='', encoding='utf-8') as f:
-                    writer = csv.writer(f)
-                    
-                    # Se o arquivo estiver vazio, adicione o cabeçalho
-                    #if os.stat(csv_file).st_size == 0:
-                        #writer.writerow(['selected_option', 'new_value', 'hop', 'ms'])  # Cabeçalho
+                    with open(csv_file, 'a', newline='', encoding='utf-8') as f:
+                        writer = csv.writer(f)
 
-                    # Escreve os dados na lista no arquivo CSV
-                    writer.writerows(data_to_write)
+                        # Escreve os dados na lista no arquivo CSV
+                        writer.writerows(data_to_write)
--- main.py
+++ main.py
@@ -1,53 +1,21 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 import os
-from functions import save_options, select_source, test_and_save_image
+from functions import save_options, select_source, test_dados
+from selenium import webdriver
 
 def main():
-    # Especifique o diretório de destino
-    download_directory = r"C:\Users\zelin\Documents\SeleniumProjeto\images"  # Examplo: r"C:\Users\Maquinado\Documents\SeleniumProjeto\images"
-
-    # Certifique-se de que o diretório de destino exista
-    if not os.path.exists(download_directory):
-        os.makedirs(download_directory)
-
-    # Configurar as opções do Chrome para fazer o download no diretório especificado
     chrome_options = webdriver.ChromeOptions()
-    prefs = {
-        "download.default_directory": download_directory,
-        "download.prompt_for_download": False,
-        "download.directory_upgrade": True,
-        "safebrowsing.enabled": True
-    }
-    chrome_options.add_experimental_option("prefs", prefs)
-
-    # Inicializar o driver do Chrome com as opções personalizadas
     driver = webdriver.Chrome(options=chrome_options)
-
-    # Navegar até o site
     driver.get("http://isp.tools/traceroute")
-
-    # Esperar um momento para o site carregar completamente
     time.sleep(5)
+    opcoes_origem = save_options(driver)  # Salvar as opções de origem em um arquivo CSV
 
-    # Extrair e salvar as opções de origem em um arquivo CSV
-    opcoes_origem = save_options(driver)
-
-    # Escolher uma origem e testar o IP
-    selected_option = select_source(driver, opcoes_origem)
-    if selected_option:
-        test_and_save_image(driver, selected_option)
-
-    # Fechar o navegador
+    for i in range(len(opcoes_origem)):
+        selected_option = select_source(driver, opcoes_origem, i)
+        if selected_option:
+            test_dados(driver, selected_option)
+    
     driver.quit()
 
 if __name__ == '__main__':
     main()
-
-
-
-#Pegar o ms e o hop
